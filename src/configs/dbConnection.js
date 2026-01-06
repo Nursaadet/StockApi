81b76524a488305d3137210cea566aee7aee78f2
Nursaadet <nursaadetozdemir@gmail.com>
2026-01-06 23:39:19 +0300

    fix(config): remove deprecated mongoose options and fix env key

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -2,19 +2,14 @@
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-// MongoDB Connection:
 
 const mongoose = require("mongoose");
 
 const dbConnection = function () {
-  // Connect:
   mongoose
-    .connect(process.env.MONGODB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => console.log("* DB Connected * "))
-    .catch((err) => console.log("* DB Not Connected * ", err));
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("* DB Connected *"))
+    .catch((err) => console.log("* DB Not Connected *", err));
 };
 
 /* ------------------------------------------------------- */
