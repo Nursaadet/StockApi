99905bc9d386c1f6831113cbfd14ece76526371f
Nursaadet <nursaadetozdemir@gmail.com>
2026-01-06 16:52:27 +0300

    feat(firm): add Firm model

--- src/models/firm.js
+++ src/models/firm.js
@@ -3,4 +3,39 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require("../configs/dbConnection");
+/* ------------------------------------------------------- *
+{
+    "name": "Firm -1"
+}
 /* ------------------------------------------------------- */
+// Firm Model:
+
+const FirmSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    phone: {
+      type: String,
+      trim: true,
+    },
+
+    address: {
+      type: String,
+      trim: true,
+    },
+
+    image: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    collection: "firms",
+    timestamps: true,
+  }
+);
