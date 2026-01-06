0a468267f05363b45b9b21022149d85a14c9e6bd
Nursaadet <nursaadetozdemir@gmail.com>
2026-01-06 16:13:49 +0300

    feat(routes): mount User routes at /users

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,3 +4,22 @@
 ------------------------------------------------------- */
 const router = require("express").Router();
 /* ------------------------------------------------------- */
+// routes/user:
+
+const user = require('../controllers/user')
+
+// URL: /users
+
+router.route('/')
+    .get(user.list)
+    .post(user.create)
+
+router.route('/:id')
+    .get(user.read)
+    .put(user.update)
+    .patch(user.update)
+    .delete(user.delete)
+
+/* ------------------------------------------------------- */
+// Exports:
+module.exports = router
